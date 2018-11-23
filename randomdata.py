fe8f46ee31c0c18622af267c596d0b53f2e7c782
Kazuki Nakajima <nakajima.k0301@gmail.com>
2018-11-23 11:25:04 +0900

    リアルタイム検索を実装

--- randomdata.py
+++ randomdata.py
@@ -6,12 +6,12 @@ def randname(n):
     randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
     return ''.join(randlst)
 
-for i in range(1000):
+for i in range(100):
     name = randname(10)
     print("{", end='')
     print("id: {},".format(i), end='')
     print(" name: '{}',".format(name), end='')
     print(" kana: '{}',".format(name), end='')
     print(" roma: '{}',".format(name), end='')
-    print(" wattage: '{}'".format(random.randint(0,1000)), end='')
+    print(" wattage: '{}'".format(round(random.uniform(0,1000), 2)), end='')
     print("},")
