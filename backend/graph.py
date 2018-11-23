6e2fe1a06264da699253ec4eb8d4f5e61d7592d4
Kazuki Nakajima <nakajima.k0301@gmail.com>
2018-11-23 18:14:46 +0900

    update

--- backend/graph.py
+++ backend/graph.py
@@ -14,12 +14,14 @@ conn = mysql.connector.connect(
     password = url.password,
     database = url.path[1:],
 )
+cur = conn.cursor()
+cur.execute('SELECT wattage from sensors where id=11 and created_at between "2018-11-23 00:00:00" and "2018-11-24 00:00:00"')
 
-print(conn.is_connected())
+t = cur.fetchall()
 
-x = np.arange(0, 10, 0.1)
-y_tan = np.tan(x)
-plt.plot(x, y_tan)
+x = np.arange(0, 1000, 1)
+y = t
+plt.plot(x, y)
 plt.gca().spines['top'].set_visible(False)
 plt.gca().spines['bottom'].set_visible(False)
 plt.gca().spines['right'].set_visible(False)
